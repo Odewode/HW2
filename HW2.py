4dc840130822256a3cb17473a039674cdc4b94cd
TamerlanMuratuly <odewed@gmail.com>
2022-11-29 01:47:06 +0600

    Create HW2.py

---
+++ HW2.py
@@ -0,0 +1,65 @@
+# 1.
+# val1 = int(input("Pervoe chislo"))
+# val2 = int(input("Vtoroe chislo"))
+# val3 = int(input("Tretye chislo"))
+# summa = val1+val2+val3
+# proizvedenie = val1*val2*val3
+# print("summa", summa)
+# print("proizvedenie", proizvedenie)
+
+# 2.
+# val1 = int(input("Zarplata"))
+# val2 = int(input("Bank"))
+# val3 = int(input("Kom"))
+# ostatok = val1-val2-val3
+# print("ostatok", ostatok)
+
+# 3.
+# d1 = float(input("pervaya diagonal"))
+# d2 = float(input("vtoraya diagonal"))
+# s = d1*d2*0.5
+# print("ploshat", s)
+
+# 4.
+# print("To be \nor not\nto be")
+
+# 5.
+# print("'Life is what happens\n    when\n       you're busy making other plans'\n                           "
+#       "         John Lennon.")
+
+# 6.
+# d = int(input("Diametr"))
+# Pi = 3.14
+# L = Pi*d
+# print("Dlina", L)
+
+# 7.
+# R = int(input("radius"))
+# Pi = 3.14
+# L = 2*Pi*R
+# S = Pi*R**2
+# print("okruzhnost", L)
+# print("ploshat", S)
+
+# 8.
+# a = int(input("chislo 1"))
+# b = int(input("chislo 2"))
+# kvkoren = (a*b)**0.5
+# print("kvadratnykoren", kvkoren)
+
+# 9.
+# L = int(input(dlina)) 2*Pi*R
+# Pi = 3.14
+# S = Pi*R**2
+# print("radius", R)
+# print("ploshat", S)
+
+# 10.
+# x = int(input("x"))
+# y = 3*x**2-6*x-7
+# print("y", y)
+
+# 11.
+# x = int(input("x"))
+# y = 4*(x-3)-7*(x-3)+2
+# print("y", y)
